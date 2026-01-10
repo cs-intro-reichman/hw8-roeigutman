8f718d07777009837bfbe909bac911c06325c942
roei gutman <git config --global user.email david.german@post.runi.ac.il>
2026-01-10 20:07:27 +0200

    Completed assignment

--- User.java
+++ User.java
@@ -83,17 +83,21 @@
     /** Removes the given name from the follows list of this user. If successful, returns true.
      *  If the name is not in the list, does nothing and returns false. */
     public boolean removeFollowee(String name) {
-        
+         if (name == null) 
+            return false;
+            
+         
          for(int i = 0; i < fCount; i++){
              if (follows[i].equals(name)) {
              
                 for(int j = i; j < fCount - 1; j++){
                  follows[j] = follows[j + 1];
-                fCount--;
+                }
+                 
                 follows[fCount - 1] = null;
                 fCount--;
                 return true;
-                }
+               
              }
             } 
          
